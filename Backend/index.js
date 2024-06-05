e10b91f9426377896730b114955a0f6c933c6e49
jenilparmar <jerryhash231@gmail.com>
2024-06-06 00:50:02 +0530

    xz

--- Backend/index.js
+++ Backend/index.js
@@ -11,7 +11,7 @@ const {
   getImageL4,
 } = require("./likeConfiguration");
 
-const PORT = 5000;
+const PORT = process.env.PORT ||5000;
 const app = express();
 url = process.env.connectionString;
 const dbName = "MemeMenia";
